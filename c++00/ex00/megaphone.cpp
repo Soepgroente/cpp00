9ee7099feac60133381a0d1804788ee252a56bbc
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2023-11-29 19:59:37 +0100

    exercise 00

--- c++00/ex00/megaphone.cpp
+++ c++00/ex00/megaphone.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <string>
 
-void	PrintCapsString(std::string input)
+using namespace std;
+
+void	ShoutInput(string &input)
 {
-	std::string UpperCased;
-
 	for (int i = 0; input[i] != '\0'; i++)
-		UpperCased[i] = toupper(input[i]);
-	std::cout << UpperCased << "\n";
+		input[i] = toupper(input[i]);
+	cout << input << endl;
 }
 
 int32_t	main(int argc, char** argv)
 {
-	std::string input;
+	string input;
 
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 		return (0);
 	}
-	for (int i = 1; argv[i] != NULL; i++)
+	input = argv[1];
+	for (int i = 2; argv[i] != NULL; i++)
+	{
+		input += " ";
 		input.append(argv[i]);
-	PrintCapsString(input);
+	}
+	ShoutInput(input);
 }
