31f507380026b266109821aba952fa7268065116
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2024-01-24 22:44:47 +0100

    more exercises done

--- c++01/ex04/main.cpp
+++ c++01/ex04/main.cpp
@@ -1,25 +1,47 @@
-Create a program that takes three parameters in the following order: a filename and
-two strings, s1 and s2.
-It will open the file <filename> and copies its content into a new file
-<filename>.replace, replacing every occurrence of s1 with s2.
-Using C file manipulation functions is forbidden and will be considered cheating. All
-the member functions of the class std::string are allowed, except replace. Use them
-wisely!
-Of course, handle unexpected inputs and errors. You have to create and turn in your
-own tests to ensure your program works as expected.;
-
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+#define FOREVER 1
+
+void	replace_lines(std::ofstream& dest, std::ifstream& src, std::string s1, std::string s2)
+{
+	std::string	line;
+
+	while (FOREVER)
+	{
+		std::getline(src, line, '\n');
+		if (line.compare(s1) == 0)
+			line = s2;
+		dest << line;
+		if (src.eof() == true)
+			break ;
+		dest << std::endl;
+	}
+}
 
 int main(int argc, char** argv)
 {
-	std::string	filename;
-	std::string	s1;
-	std::string	s2;
-
-	if (argc < 3)
+	if (argc != 4)
 	{
 		std::cout << "Please include a filename and strings s1 & s2" << std::endl;
 		return (1);
 	}
-	
+	std::ofstream	dest;
+	std::string		s1 = argv[2];
+	std::string		s2 = argv[3];
+	std::string		file;
+	std::ifstream	src;
+
+	file = argv[1];
+	src.open(file);
+	if (src.is_open() == false)
+		return (2);
+	dest.open("dest.txt");
+	if (dest.is_open() == false)
+		return (3);
+	replace_lines(dest, src, s1, s2);
+	src.close();
+	dest.close();
 }
---
+++ c++01/ex05/Harl.cpp
@@ -0,0 +1,26 @@
+#include "Harl.hpp"
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having veggie bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe veggie bacon costs more money. You didn't put enough in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra salad for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	this->level()
+}
